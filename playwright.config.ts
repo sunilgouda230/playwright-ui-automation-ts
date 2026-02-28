3fb6cbfa74df3348967877cdf71acba55b65c741
sunilgouda230 <sunilgouda230@gmail.com>
2026-02-28 12:40:05 +0530

    git-hub-action check third commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,29 +1,34 @@
 import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
-
-const env = process.env.ENV || 'qa'; // default = qa
-dotenv.config({ path: `${env}.env` });
+// Load env files only for local execution
+if (!process.env.CI) {
+  const env = process.env.ENV || 'qa';
+  dotenv.config({ path: `${env}.env` });
+}
 
 export default defineConfig({
   testDir: './tests',
+
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
+
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
+
   reporter: 'html',
+
   use: {
     baseURL: process.env.BASE_URL,
+    headless: true,
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
-    headless: true,
   },
 
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    }
+    },
   ],
 });
