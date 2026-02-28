dbff1bd0576dab821eedfa648a668af492f6922b
sunilgouda230 <sunilgouda230@gmail.com>
2026-02-28 12:52:47 +0530

    added promise in login page

--- framework/pages/LoginPage.ts
+++ framework/pages/LoginPage.ts
@@ -19,11 +19,15 @@ export class LoginPage {
   await this.page.goto('/login');
 }
 
-  async login(username: string, password: string) {
-    await this.usernameInput.fill(username);
-    await this.passwordInput.fill(password);
-    await this.loginButton.click();
-  }
+ async login(username: string, password: string) {
+  await this.usernameInput.fill(username);
+  await this.passwordInput.fill(password);
+
+  await Promise.all([
+    this.page.waitForURL('**/secure'),
+    this.loginButton.click(),
+  ]);
+}
 
   getErrorMessage() {
     return this.errorMessage;
